f0167ea2cad0cd066c305b5498166da44102db5c
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-03-13 18:46:30 +0800

    Update and add main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const navLi = document.querySelectorAll('nav div ul li a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 65) {
+            current = section.getAttribute('id');
+        }
+    });
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        document.querySelector('nav div ul li a[href*= ' + current + ']').classList.add('active');
+    });
+});
