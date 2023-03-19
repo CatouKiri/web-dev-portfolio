0e82f74c4c6802e343fdf2146cb67e48a6b49e5a
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-03-19 14:15:58 +0800

    Add animation on scroll

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ window.addEventListener('scroll', () => {
         document.querySelector('nav div ul li a[href*= ' + current + ']').classList.add('active');
     });
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
